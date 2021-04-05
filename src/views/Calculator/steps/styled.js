6b8d01eed765a1155f5307ef84a13b83cb522f0d
Yusuf Özlü <y.ozlu@cleo.one>
2021-04-05 16:04:25 +0200

    fix width of calculator form

--- src/views/Calculator/steps/styled.js
+++ src/views/Calculator/steps/styled.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 export default styled.div`
   padding: 40px 40px;
   min-width: 552px;
+  max-width: 552px;
   background-color: ${({ theme }) => theme.colors.white};
   box-shadow: 0 15px 20px rgba(31, 72, 143, 0.02);
   border-radius: 20px;
